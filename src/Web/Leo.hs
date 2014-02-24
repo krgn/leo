cb248739e32c5698b210b269156cb6026df5e970
Karsten Gebbert <k@ioctl.it>
2014-02-24 23:32:32 +0100

    some docs

--- src/Web/Leo.hs
+++ src/Web/Leo.hs
@@ -23,6 +23,7 @@ import Network.HTTP
 data Language = En | Fr | Sp | It | Ch | Ru | Pt | Pl | De | Unknown
               deriving (Show, Eq)
 
+-- |For simple parsing of Language from XML we use the Read class.
 instance Read Language where
     readsPrec _ value = 
         tryParse langs 
@@ -35,36 +36,49 @@ instance Read Language where
                     then [(result, drop (length attempt) value)]
                     else tryParse xs
 
-data Translation = Translation { lang :: Language, val :: String }
+-- | A Tanslation always has a language and a value, the translation
+data Translation = Translation { 
+        language    :: Language, 
+        translation :: String 
+    }
     deriving (Show, Eq)
 
-data QueryResult = Nouns    [(Translation,Translation)]
-                 | Verbs    [(Translation,Translation)] 
-                 | AdjAdvs  [(Translation,Translation)] 
-                 | Examples [(Translation,Translation)]
+data QueryResult = Nouns    [(Translation,Translation)] -- ^ Nouns    constructor is a list of translation tuples
+                 | Verbs    [(Translation,Translation)] -- ^ Verbs    constructor is a list of translation tuples 
+                 | AdjAdvs  [(Translation,Translation)] -- ^ AdjAdvs  constructor is a list of translation tuples 
+                 | Examples [(Translation,Translation)] -- ^ Examples constructor is a list of translation tuples
                  | None
                  deriving (Show)
 
 data OutFormat = JSON | TSV | CSV
     deriving (Show, Eq)
 
-query :: String -> Language -> IO([QueryResult])
-query term lang = do
-    result <- fetch term lang
+-- |'query' runs a translation query against the dict.leo.org webservice 
+query :: String            -- ^ search term to translate
+      -> Language          -- ^ language to translate to/from 
+      -> IO([QueryResult]) -- ^ returns a list of QueryResult
+query term l = do
+    result <- fetch term l
     let categories = processRaw result
     return $ map toQueryResult categories
 
--- TODO: build the request from options (general options type? Data.Default?)
-fetch :: String -> Language -> IO (String)
+
+-- |'fetch' take a query string, some options and fetches a response from the webservice
+fetch :: String  -- ^ the query string
+      -> Language -- ^ the Language to query results for 
+      -> IO (String) -- ^ the String response
 fetch lang q = do
     let get = getRequest "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=de&rmWords=off&rmSearch=on&directN=0&search=update&searchLoc=0&resultOrder=basic&sectLenMax=16"
     result <- simpleHTTP get
     getResponseBody result
 
-processRaw :: String -> [[Tag String]]
-processRaw  = sections
-    where sections = partitions (~== TagOpen "section" []) . parseTags
 
+-- |'processRaw' takes a raw XML response, soupifies it and returns the result sections
+processRaw :: String -> [[Tag String]]
+processRaw = partitions (~== TagOpen "section" []) . parseTags
+
+
+-- |'toQueryResult' turns a section and turns into a QueryResult
 toQueryResult :: [Tag String] -> QueryResult
 toQueryResult soup = let category = head soup in 
         case (fromAttrib "sctName" category) of
@@ -76,7 +90,8 @@ toQueryResult soup = let category = head soup in
         where collectResults s = map toTranslation 
                                     $ partitions (~== TagOpen "entry" []) s
 
--- process the "entry"
+
+-- |'toTranslation' takes an entry and turns it into a Translation set
 toTranslation :: [Tag String] -> (Translation, Translation)
 toTranslation entry = (Translation a astr, Translation b bstr)
     where sides = partitions (~== TagOpen "side" []) entry
@@ -86,6 +101,7 @@ toTranslation entry = (Translation a astr, Translation b bstr)
           b     = read $ fromAttrib "lang" $ head $ last sides
           bstr  = concat $ map (fromTagText . head) 
                     $ partitions (~== TagText "" ) $ last sides
+
 
 format :: OutFormat -> [QueryResult] -> String
 format = undefined 
