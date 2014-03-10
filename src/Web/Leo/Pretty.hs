780e6bedc048e8fc1fca7b29d29287fadfe66b92
Karsten Gebbert <karsten@null2.net>
2014-03-10 12:13:40 +0100

    finally, pretty printing the results

--- src/Web/Leo/Pretty.hs
+++ src/Web/Leo/Pretty.hs
@@ -1,43 +1,44 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE OverlappingInstances   #-}
 
 module Web.Leo.Pretty where
 
 import Web.Leo.Types
-import Text.PrettyPrint.ANSI.Leijen
+import Data.List
+import Text.PrettyPrint.ANSI.Leijen ((<$>))
+import qualified Text.PrettyPrint.ANSI.Leijen as P
+import qualified Text.PrettyPrint.Boxes as B
 
-instance Pretty Language where
-    pretty De      = string "German"
-    pretty En      = string "English"
-    pretty Fr      = string "French"
-    pretty Es      = string "Spanish"
-    pretty It      = string "Italian" 
-    pretty Ch      = string "Chinese" 
-    pretty Ru      = string "Russian" 
-    pretty Pt      = string "Portugese" 
-    pretty Pl      = string "Polish"
-    pretty Unknown = string "Unknown"
+instance P.Pretty Language where
+    pretty De      = P.string "German"
+    pretty En      = P.string "English"
+    pretty Fr      = P.string "French"
+    pretty Es      = P.string "Spanish"
+    pretty It      = P.string "Italian" 
+    pretty Ch      = P.string "Chinese" 
+    pretty Ru      = P.string "Russian" 
+    pretty Pt      = P.string "Portugese" 
+    pretty Pl      = P.string "Polish"
+    pretty Unknown = P.string "Unknown"
 
-instance Pretty String where
-    pretty     s  = text s
-    prettyList ss = foldr (\n p -> pretty n <+> pretty p) empty ss
+instance P.Pretty QueryResult where
+    pretty (Nouns    xs) = prettify "Nouns:"                (boxify xs)
+    pretty (Phrase   xs) = prettify "Phases:"               (boxify xs)
+    pretty (Verbs    xs) = prettify "Verbs:"                (boxify xs)
+    pretty (AdjAdvs  xs) = prettify "Adjectives/Adverbs:"   (boxify xs)
+    pretty (Examples xs) = prettify "Examples:"             (boxify xs)
+    pretty None          = P.string "None"
 
-instance Pretty TEntry where
-    pretty e = (pretty $ getResult e)
+    prettyList xs = foldr (\n p -> P.pretty n <$> P.pretty p) P.empty xs
 
-instance Pretty (TEntry,TEntry) where
-    pretty (l,r) = fill 10 leftE <+> rightE 
-        where leftE  = pretty $ getResult l
-              rightE = pretty $ getResult r
-    prettyList xs = foldr (\n p -> pretty n <+> fill 40 empty <+> pretty p) empty xs
+prettify :: String -> B.Box -> P.Doc
+prettify a b = 
+    P.underline (P.bold (P.text a)) 
+    <$> P.linebreak 
+    <$>P.indent 4 (P.string $ B.render b)
 
-instance Pretty QueryResult where
-    pretty (Nouns    xs) = underline (bold (text "Nouns:"))    <$> indent 4 (vsep (map pretty xs))
-    pretty (Phrase   xs) = underline (bold (text "Phrases:"))  <$> indent 4 (vsep (map pretty xs))
-    pretty (Verbs    xs) = underline (bold (text  "Verbs:"))   <$> indent 4 (vsep (map pretty xs))
-    pretty (AdjAdvs  xs) = underline (bold (text "AdjAdvs:"))  <$> indent 4 (vsep (map pretty xs))
-    pretty (Examples xs) = underline (bold (text "Examples:")) <$> indent 4 (vsep (map pretty xs))
-    pretty None          = string "None"
-
-    prettyList xs = foldr (\n p -> pretty n <$> pretty p) empty xs
-
+boxify :: [(TEntry,TEntry)] -> B.Box
+boxify xs = box
+    where cols  = transpose $ map (\(l,r) -> [procR l, procR r]) xs
+          box   = B.hsep 2 B.left $ map (B.vcat B.left . map B.text) cols
+          procR s = intercalate ", " $ getResult s
